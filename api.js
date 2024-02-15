24b4e1a700c6dac136682d19de70886f2bc1b950
0xfc002 <159228504+0xfc002@users.noreply.github.com>
2024-02-15 09:55:29 -0600

    Create api.js

---
+++ api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+
+// TODO: Make config-driven
+const HOST = window.location.href.indexOf('http://localhost:') === 0 ? (
+  'http://localhost:8000'
+) : (
+  'https://farcoin.xyz'
+);
+
+export const scanMints = async params => {
+  const response = await axios.get(`${HOST}/scan`, { params });
+  return response.data.results;
+}
+
+export const signMints = async params => {
+  const response = await axios.get(`${HOST}/sign`, { params });
+  return response.data.results;
+}
+
+export const startSession = async (params) => {
+  const response = await axios.post(`${HOST}/session/start`, params);
+  return response.data.results;
+}
+
+export const endSession = async () => {
+  const response = await axios.post(`${HOST}/session/end`);
+  return response.data.results;
+}
+
+export const getSessionCode = async () => {
+  const response = await axios.post(`${HOST}/session/code`);
+  return response.data.results;
+}
+
+export const getSession = async params => {
+  const response = await axios.get(`${HOST}/session`, { params });
+  return response.data.results;
+}
