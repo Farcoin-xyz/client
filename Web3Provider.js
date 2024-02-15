f5e52f04f1f988cc965a6023b728f55acc93d349
0xfc002 <159228504+0xfc002@users.noreply.github.com>
2024-02-15 09:56:40 -0600

    Create Web3Provider.js

---
+++ Web3Provider.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { WagmiProvider, http, createConfig } from "wagmi";
+import { base } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { injected, metaMask, safe, walletConnect } from "wagmi/connectors"
+
+const config = createConfig(
+  getDefaultConfig({
+    // Your dApps chains
+    chains: [base],
+    transports: {
+      // RPC URL for each chain
+      [base.id]: http(
+        "https://base.publicnode.com",
+      ),
+    },
+
+    // Required API Keys
+    walletConnectProjectId: "390c9947a2e2ce942088081c0f47c8c8",
+
+    // Required App Info
+    appName: "Farcoin",
+
+    // Optional App Info
+    // appDescription: "Your App Description",
+    // appUrl: "https://family.co", // your app's url
+    // appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  }),
+);
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
